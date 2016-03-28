bd6c4160c26dcd39487255d41036a014e7a1a3f5
Dave Yan <fdu5526@live.com>
2016-03-28 16:19:05 -0700

    water

---
+++ Assets/Resources/Scripts/Whale.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Whale : Physics2DBody {
+
+	float speed;
+
+	// Use this for initialization
+	void Awake () {
+		base.Awake();
+		speed = 2f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rigidbody2d.velocity = Vector2.left * speed;
+	}
+}
