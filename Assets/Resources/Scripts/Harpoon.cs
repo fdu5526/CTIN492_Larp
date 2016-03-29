5fffa0018f98eb203643c1939a0f734121debb49
Dave Yan <fdu5526@live.com>
2016-03-28 20:28:17 -0700

    yay

--- Assets/Resources/Scripts/Harpoon.cs
+++ Assets/Resources/Scripts/Harpoon.cs
@@ -10,6 +10,7 @@ public class Harpoon : Physics2DBody {
 
 	float speed = 20f;
 	bool firing;
+	ParticleSystem bloodPS;
 
 	// Use this for initialization
 	protected override void Awake () {
@@ -19,18 +20,25 @@ public class Harpoon : Physics2DBody {
 		lifespan = maxLifespan;
 		firing = false;
 		audios = GetComponents<AudioSource>();
+		bloodPS = transform.Find("BloodPS").GetComponent<ParticleSystem>();
+	}
+
+	void DestroySelf () {
+		collider2d.enabled = false;
+		Destroy(this.gameObject, 3f);
 	}
 
 	// stop moving, and become unhittable
 	void HitSomething () {
 		firing = false;
-		collider2d.enabled = false;
+		bloodPS.GetComponent<ParticleSystem>().Play();
 		rigidbody2d.velocity = Vector2.zero;
 		rigidbody2d.angularVelocity = 0f;
+		Invoke("DestroySelf", 0.4f);
 	}
 
 	void OnCollisionEnter2D (Collision2D coll) {
-		if (coll.gameObject != null) {
+		if (firing && coll.gameObject != null) {
 			HitSomething();
 		}
 	}
--- Assets/Resources/Scripts/Whale.cs
+++ Assets/Resources/Scripts/Whale.cs
@@ -5,14 +5,12 @@ public class Whale : Physics2DBody {
 
 	float speed;
 	AudioSource[] audios;
-	ParticleSystem bloodPS;
 
 	// Use this for initialization
 	void Awake () {
 		base.Awake();
 		speed = 2f;
 		audios = GetComponents<AudioSource>();
-		bloodPS = transform.Find("BloodPS").GetComponent<ParticleSystem>();
 	}
 
 	void OnCollisionEnter2D (Collision2D coll) {
@@ -20,13 +18,13 @@ public class Whale : Physics2DBody {
 		if (l == Global.LayerPlayer) {
 			int r = (int)UnityEngine.Random.Range(0f, 3f);
 			audios[r].Play();
-
-			bloodPS.GetComponent<ParticleSystem>().Play();
 		}
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		rigidbody2d.velocity = Vector2.left * speed;
+		Vector2 v = rigidbody2d.velocity;
+		v.x = -speed;
+		rigidbody2d.velocity = v;
 	}
 }
